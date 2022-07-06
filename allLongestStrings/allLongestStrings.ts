0a3144e93f215aae84e796cf4614a664553bfcaf
Alexander <Alexander_alatorre1993@hotmail.com>
2022-07-05 22:01:20 -0600

    solved another algo

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,20 @@
 export function allLongestStrings(inputArray: string[]): string[] {
+  if (inputArray.length === 1) return inputArray;
+  
+  let longestValue = inputArray[0];
+  const longestStrings = [];
 
+  for (let i = 0; i < inputArray.length; i++) {
+    if (inputArray[i].length > longestValue.length) {
+      longestValue = inputArray[i];
+    }
+
+    if (longestValue.length === inputArray[i].length) {
+      longestStrings.push(inputArray[i]);
+    }
+  }
+
+  return longestStrings;
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
