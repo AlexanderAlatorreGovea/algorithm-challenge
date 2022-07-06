2404b345e2b60fa15e7a001a2c6587ddefcbacc2
Alexander <Alexander_alatorre1993@hotmail.com>
2022-07-05 22:11:35 -0600

    solved another algo:

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,18 @@
 export function almostIncreasingSequence(sequence: number[]): boolean {
+  let isASequence = false;
 
+  for (let i = 0; i < sequence.length; i++) {
+    const nextNumber = sequence[i + 2];
+
+    if (!nextNumber) break;
+
+    if (sequence[i] + 1 === nextNumber) {
+      isASequence = true;
+    }
+  }
+
+  return isASequence;
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2])) 
+console.log(almostIncreasingSequence([1, 3, 2, 1]));
+ console.log(almostIncreasingSequence([1, 3, 2]))
