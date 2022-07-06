27ab5bcab45232bd27eac5950160f6d702bae733
Alexander <Alexander_alatorre1993@hotmail.com>
2022-07-05 21:23:44 -0600

    solved another algo

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,18 @@
 export function adjacentElementsProduct(inputArray: number[]): number {
+  if (inputArray.length === 1) return inputArray[0];
 
+  const products = [];
+
+  for (let i = 0; i < inputArray.length; i++) {
+    const adjacentProducts = inputArray[i] * inputArray[i + 1];
+
+    if (!adjacentProducts) break;
+
+    products.push(adjacentProducts);
+  }
+
+  return products[products.length - 1];
 }
+console.log(adjacentElementsProduct([2, 6,]));
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
