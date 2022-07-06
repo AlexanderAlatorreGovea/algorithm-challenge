0abeda53c01942718f20b0c587a8938e6690ff4e
Alexander <Alexander_alatorre1993@hotmail.com>
2022-07-05 21:13:51 -0600

    solved another algo

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,19 @@
-export function addBorder(picture: string[]): string[] {
+export function addBorder(picture: string[]): any {
+    const lengthOfWall = picture[0].length + 2;
+    let wall = "";
 
+  for (let i = 0; i < lengthOfWall; i++) {
+    wall = wall.concat("*");
+  }
+
+  picture.unshift(wall);
+  picture.push(wall);
+
+  for (let i = 1; i < picture.length - 1; i++) {
+    picture[i] = "*".concat(picture[i], "*");
+  }
+
+  return picture;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,14 @@
 export function addTwoDigits(n: any): number {
+  if (n.length === 1) return n;
 
+  let sum = 0;
+  const numbers = n.toString().split('')
+
+  for (let i = 0; i < numbers.length; i++) {
+      sum += +numbers[i]
+  }
+
+  return sum;
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
