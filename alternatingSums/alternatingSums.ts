1de5b9836be0e799f02f4ce83af96eb1ea35775f
Alexander <Alexander_alatorre1993@hotmail.com>
2022-07-06 19:40:23 -0600

    solved algo

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,16 @@
 export function alternatingSums(a: number[]): number[] {
+  let teamOnesWeight = 0;
+  let teamTwosWeight = 0;
 
+  for (let i = 0; i < a.length; i++) {
+    if (i % 2 === 0) {
+      teamOnesWeight += a[i];
+    } else {
+      teamTwosWeight += a[i];
+    }
+  }
+
+  return [teamOnesWeight, teamTwosWeight];
 }
 
-console.log(alternatingSums([50, 60, 60, 45, 70]))
+console.log(alternatingSums([50, 60, 60, 45, 70]));
