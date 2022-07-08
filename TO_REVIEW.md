ac67880c64edbc37d90e20fea2cfa966638f729a
Alexander <Alexander_alatorre1993@hotmail.com>
2022-07-07 21:18:53 -0600

    solved another algo

--- TO_REVIEW.md
+++ TO_REVIEW.md
@@ -9,3 +9,4 @@ arrayConversion (tracking odd values)
 
 // Javascript methods
 1. chartCodeAt(0) to get the index of a letter.
+2. math.abs()
--- arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
+++ arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
@@ -1,5 +1,12 @@
 export function arrayMaximalAdjacentDifference(inputArray: number[]): number {
+  let maximumValue = 0;
 
+  for (let i = 0; i < inputArray.length; i++) {
+    if (!inputArray[i + 1]) break;
+    maximumValue = Math.abs(inputArray[i] - inputArray[i + 1]);
+  }
+
+  return Math.abs(maximumValue);
 }
 
-// console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
+console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
