ad5c7448b93fa0b34b59d22b34229b81be35cfb9
Alexander <Alexander_alatorre1993@hotmail.com>
2022-07-07 19:34:10 -0600

    solved algo

--- arrayConversion/arrayConversion.ts
+++ arrayConversion/arrayConversion.ts
@@ -1,5 +1,30 @@
 export function arrayConversion(inputArray: number[]): number {
+  //checks if it is odd on every iteration
+  let isOdd = true;
 
+  while (inputArray.length !== 1) {
+    inputArray = sumProduct(inputArray, isOdd);
+
+    isOdd = !isOdd;
+  }
+
+  return inputArray[0];
 }
 
-// console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
+function sumProduct(nums, isOdd) {
+  const sumProducts = [];
+
+  if (isOdd) {
+    for (let i = 0; i < nums.length; i += 2) {
+      sumProducts.push(nums[i] + nums[i + 1]);
+    }
+  } else {
+    for (let i = 0; i < nums.length; i += 2) {
+      sumProducts.push(nums[i] * nums[i + 1]);
+    }
+  }
+
+  return sumProducts;
+}
+
+console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
