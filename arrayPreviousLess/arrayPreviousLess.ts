87a64b81ebce420c4483121fdee6455dd7b0df43
Alexander <Alexander_alatorre1993@hotmail.com>
2022-07-07 21:26:55 -0600

    solved another algo

--- arrayPreviousLess/arrayPreviousLess.ts
+++ arrayPreviousLess/arrayPreviousLess.ts
@@ -1,5 +1,16 @@
 export function arrayPreviousLess(items: number[]): number[] {
+  const newArray = [];
+  for (let index = 0; index < items.length; index++) {
+    const element = items[index];
 
+    if (element < items[index + 1] && element) {
+      newArray.push(element);
+    } else {
+      newArray.push(-1);
+    }
+  }
+
+  return newArray
 }
 
-// console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
+console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
