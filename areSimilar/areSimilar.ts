572ab5b59a0ff3bda917f00780d15a13dd67b189
Alexander <Alexander_alatorre1993@hotmail.com>
2022-07-07 18:54:02 -0600

    completed areSimilar algo

--- areSimilar/areSimilar.ts
+++ areSimilar/areSimilar.ts
@@ -1,7 +1,17 @@
-export function areSimilar(a: number[], b: number[]): boolean {
+export function areSimilar(aNums: number[], bNums: number[]): boolean {
+  let moreThanOneMatch = 0;
 
+  for (let index = 0; index < aNums.length; index++) {
+    for (let j = 0; j < bNums.length; j++) {
+      if (aNums[index] === bNums[j]) {
+        moreThanOneMatch += 1
+      }
+    }
+  }
+
+  return moreThanOneMatch === 1 ? true : false;
 }
 
-// console.log(areSimilar([1, 2, 3], [1, 2, 3]));
-// console.log(areSimilar([1, 2, 3], [2, 1, 3]));
-// console.log(areSimilar([1, 2, 2], [2, 1, 1]));
+console.log(areSimilar([1, 2, 3], [1, 2, 3]));
+console.log(areSimilar([1, 2, 3], [2, 1, 3]));
+console.log(areSimilar([1, 2, 2], [2, 1, 1]));
