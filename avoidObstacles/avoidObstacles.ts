d114aaad9259303a9c72ef1432e56cdd2736ca6f
Alexander <Alexander_alatorre1993@hotmail.com>
2022-07-08 22:41:16 -0600

    solved another algo

--- avoidObstacles/avoidObstacles.ts
+++ avoidObstacles/avoidObstacles.ts
@@ -1,5 +1,15 @@
 export function avoidObstacles(inputArray: number[]): number {
+  const max = Math.max(...inputArray);
 
+  for (let i = 1; i < max; i++) {
+    const divs = inputArray.some((element) => {
+      return element % i === 0;
+    });
+
+    if (!divs) return i;
+  }
+
+  return max + 1;
 }
 
-// console.log(avoidObstacles([5, 3, 6, 7, 9]));
+console.log(avoidObstacles([5, 3, 6, 7, 9]));
