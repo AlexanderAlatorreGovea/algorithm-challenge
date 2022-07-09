c963a22c50502869967fe08179cf879d99ad663f
Alexander <Alexander_alatorre1993@hotmail.com>
2022-07-08 23:30:49 -0600

    solved another algo

--- caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -1,6 +1,12 @@
 export function isCaseInsensitivePalindrome(inputString: string): boolean {
+  let palindrome = "";
 
+  for (let char of inputString) {
+    palindrome = char + palindrome;
+  }
+
+  return palindrome.toLowerCase() === inputString.toLowerCase();
 }
 
-// console.log(isCaseInsensitivePalindrome('AaBaa'));
-// console.log(isCaseInsensitivePalindrome('abac'));
+console.log(isCaseInsensitivePalindrome("AaBaa"));
+console.log(isCaseInsensitivePalindrome("abac"));
