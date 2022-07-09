1095f7a31b866c2b3f316ebee492b32768b832a0
Alexander <Alexander_alatorre1993@hotmail.com>
2022-07-08 22:01:37 -0600

    solved algo

--- arrayReplace/arrayReplace.ts
+++ arrayReplace/arrayReplace.ts
@@ -1,5 +1,20 @@
-export function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
+export function arrayReplace(
+  inputArray: number[],
+  elemToReplace: number,
+  substitutionElem: number
+): number[] {
+  const newArray = [];
 
+  for (let i = 0; i < inputArray.length; i++) {
+    if (inputArray[i] === elemToReplace) {
+      newArray.push(substitutionElem);
+      continue
+    }
+
+    newArray.push(inputArray[i]);
+  }
+
+  return newArray;
 }
 
-// console.log(arrayReplace([1, 2, 1], 1, 3));
+console.log(arrayReplace([1, 2, 1], 1, 3));
