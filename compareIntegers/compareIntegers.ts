5cd38a32ae5faad30bd8e8cca90f1d37457ead22
Alexander <Alexander_alatorre1993@hotmail.com>
2022-07-09 20:26:31 -0600

    solved algo"

--- compareIntegers/compareIntegers.ts
+++ compareIntegers/compareIntegers.ts
@@ -1,7 +1,15 @@
 export function compareIntegers(a: string, b: string): string {
+  const less = "less";
+  const greater = "greater";
+  const equal = "equal";
 
+  if (a < b) return less;
+  
+  if (b < a) return greater;
+
+  return equal;
 }
 
-// console.log(compareIntegers('12', '13'));
-// console.log(compareIntegers('875', '799'));
-// console.log(compareIntegers('1000', '1000'));
+console.log(compareIntegers('12', '13'));
+console.log(compareIntegers('875', '799'));
+console.log(compareIntegers('1000', '1000'));
