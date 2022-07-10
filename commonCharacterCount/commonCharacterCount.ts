e50e4aa2dd6e48853aa0c6486f770d36382c1ab2
Alexander <Alexander_alatorre1993@hotmail.com>
2022-07-09 20:02:03 -0600

    solved another algo :

--- commonCharacterCount/commonCharacterCount.ts
+++ commonCharacterCount/commonCharacterCount.ts
@@ -1,5 +1,35 @@
 export function commonCharacterCount(s1: string, s2: string): number {
+  const firstString = createKeyValuePair(s1);
+  const secondString = createKeyValuePair(s2);
+  let numbersInCommon = 0;
 
+  for (let char in firstString) {
+    if (secondString[char]) {
+      const firstNumberToAdd =
+        firstString[char] > secondString[char]
+          ? secondString[char]
+          : firstString[char];
+
+      numbersInCommon += firstNumberToAdd;
+    }
+  }
+
+  return numbersInCommon;
 }
 
-// console.log(commonCharacterCount('aabcc', 'adcaa'));
+function createKeyValuePair(string) {
+  const chars = {};
+
+  for (let char of string) {
+    if (!chars[char]) {
+      chars[char] = 1;
+      continue
+    }
+
+    chars[char]++;
+  }
+
+  return chars;
+}
+
+console.log(commonCharacterCount("aabcc", "adcaa"));
