cceeb096c7d43e22eb8e32bb05efe69e581d17be
Alexander <Alexander_alatorre1993@hotmail.com>
2022-07-11 19:24:39 -0600

    solved algo

--- crossingSum/crossingSum.ts
+++ crossingSum/crossingSum.ts
@@ -1,7 +1,27 @@
 export function crossingSum(matrix: number[][], a: number, b: number): number {
+  let rowSum = 0;
 
+  for (let i = 0; i < matrix[a].length; i++) {
+    rowSum += matrix[a][i];
+  }
+
+  for (let i = 0; i < matrix.length; i++) {
+    if (i !== a) {
+      rowSum += matrix[i][b];
+    }
+  }
+
+  return rowSum;
 }
 
-// console.log(crossingSum([[1, 1, 1, 1], 
-//     [2, 2, 2, 2], 
-//     [3, 3, 3, 3]], 1, 3));
+console.log(
+  crossingSum(
+    [
+      [1, 1, 1, 1],
+      [2, 2, 2, 2],
+      [3, 3, 3, 3],
+    ],
+    1,
+    3
+  )
+);
