ef302761d1748098a13613a2beefef72369c8f32
Alexander <Alexander_alatorre1993@hotmail.com>
2022-07-11 18:12:48 -0600

    solved algo

--- convertCelsiusToFahrenheit/convertCelsiusToFahrenheit.ts
+++ convertCelsiusToFahrenheit/convertCelsiusToFahrenheit.ts
@@ -1,7 +1,9 @@
 export function celsiusToFahrenheit(celsius: number): number {
+  const tempInFahrenheit = celsius * (9 / 5) + 32;
 
+  return tempInFahrenheit;
 }
 
-// console.log(celsiusToFahrenheit(-30));
-// console.log(celsiusToFahrenheit(-10));
-// console.log(celsiusToFahrenheit(0));
+console.log(celsiusToFahrenheit(-30));
+console.log(celsiusToFahrenheit(-10));
+console.log(celsiusToFahrenheit(0));
