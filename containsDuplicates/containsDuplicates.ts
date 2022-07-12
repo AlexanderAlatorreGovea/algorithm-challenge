86449822916d8dd3e8b2e933d1b1391f4dec3e4c
Alexander <Alexander_alatorre1993@hotmail.com>
2022-07-11 18:08:52 -0600

    fixed issue

--- containsDuplicates/containsDuplicates.ts
+++ containsDuplicates/containsDuplicates.ts
@@ -1,6 +1,27 @@
-export function containsDuplicates(a: number[]): boolean {
+function generateKeyValuePair(array) {
+  const obj = {};
+  for (let num of array) {
+    if (!obj[num]) {
+      obj[num] = 1;
+    } else {
+      obj[num]++;
+    }
+  }
 
+  return obj;
 }
 
-// console.log(containsDuplicates([1, 2, 3, 1]));
-// console.log(containsDuplicates([3, 1]));
+export function containsDuplicates(a: number[]): boolean {
+  const arrayAsObject = generateKeyValuePair(a);
+
+  for (let character in arrayAsObject) {
+    if (arrayAsObject[character] > 1) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+console.log(containsDuplicates([1, 2, 3, 1]));
+console.log(containsDuplicates([3, 1, 1]));
