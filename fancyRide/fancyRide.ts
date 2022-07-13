a33e3ba84f4c1b16cc8908042342eb18b8d8106a
Alexander <Alexander_alatorre1993@hotmail.com>
2022-07-12 18:29:28 -0600

    solved algo

--- fancyRide/fancyRide.ts
+++ fancyRide/fancyRide.ts
@@ -1,5 +1,16 @@
 export function fancyRide(l: number, fares: number[]): string {
+  const rides = ["UberX", "UberXL", "UberPlus", "UberBlack", "UberSUV"];
+  let chosenRides = [];
 
+  for (let i = 0; i < fares.length; i++) {
+    const ridePrice = fares[i] * l;
+
+    if (ridePrice < 20) {
+      chosenRides.push(fares[i]);
+    }
+  }
+
+  return rides[chosenRides.length - 1];
 }
 
-// console.log(fancyRide(30, [0.3, 0.5, 0.7, 1, 1.3]));
+console.log(fancyRide(30, [0.3, 0.5, 0.7, 1, 1.3]));
