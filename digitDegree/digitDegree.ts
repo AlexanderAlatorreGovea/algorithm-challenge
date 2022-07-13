96a9dcd09d4e9d028fc4759f05ec163d554f8cac
Alexander <Alexander_alatorre1993@hotmail.com>
2022-07-12 18:13:40 -0600

    solved algo

--- digitDegree/digitDegree.ts
+++ digitDegree/digitDegree.ts
@@ -1,7 +1,16 @@
 export function digitDegree(n: number): number {
+  let newNumber = 0;
 
+  if (n < 10) return n;
+
+  const num = n.toString().split("");
+  for (let i = 0; i < num.length; i++) {
+    newNumber += parseInt(num[i]);
+  }
+ 
+  return newNumber;
 }
 
-// console.log(digitDegree(5));
-// console.log(digitDegree(10));
-// console.log(digitDegree(91));
+//console.log(digitDegree(5));
+console.log(digitDegree(10));
+console.log(digitDegree(91));
--- extractMatrixColumn/extractMatrixColumn.ts
+++ extractMatrixColumn/extractMatrixColumn.ts
@@ -1,5 +1,24 @@
-export function extractMatrixColumn(matrix: number[][], column: number): number[] {
+export function extractMatrixColumn(
+  matrix: number[][],
+  column: number
+): number[] {
+  const result = [];
+  for (let i = 0; i < matrix.length; i++) {
+    if (matrix[i][column] || matrix[i][column] === 0) {
+      result.push(matrix[i][column]);
+    }
+  }
 
+  return result;
 }
 
-// console.log(extractMatrixColumn([[1, 1, 1, 2], [0, 5, 0, 4], [2, 1, 3, 6]], 2));
+console.log(
+  extractMatrixColumn(
+    [
+      [1, 1, 1, 2],
+      [0, 5, 0, 4],
+      [2, 1, 3, 6],
+    ],
+    2
+  )
+);
