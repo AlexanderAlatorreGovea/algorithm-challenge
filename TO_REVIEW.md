23d549bbe1b71023e9bd221a5b5e27dbd649fe5a
Alexander <Alexander_alatorre1993@hotmail.com>
2022-07-12 18:18:08 -0600

    solved algo

--- TO_REVIEW.md
+++ TO_REVIEW.md
@@ -10,6 +10,7 @@ confirm ending
 containsCloseNumbers
 convertString (checks if every character happens in the other array)
 depositProfit (while loop)
+extractMatrixColumn
 
 // Javascript methods
 1. chartCodeAt(0) to get the index of a letter.
--- factorializeANumber/factorializeANumber.ts
+++ factorializeANumber/factorializeANumber.ts
@@ -1,6 +1,12 @@
 export function factorializeANumber(num: number): number {
+  let sum = 1;
 
+  for (let i = 1; i < num + 1; i++) {
+    sum *= i;
+  }
+
+  return sum;
 }
 
-// console.log(factorializeANumber(5));
-// console.log(factorializeANumber(10));
+console.log(factorializeANumber(5));
+console.log(factorializeANumber(10));
