aa017029caf03e3132febf4045642c05d8c29937
Alexander <Alexander_alatorre1993@hotmail.com>
2022-07-14 19:03:31 -0600

    flatened an array

--- TO_REVIEW.md
+++ TO_REVIEW.md
@@ -13,6 +13,7 @@ depositProfit (while loop)
 extractMatrixColumn
 fermactor
 findClosestPair
+flattenArray (flaten an array)
 
 // Javascript methods
 1. chartCodeAt(0) to get the index of a letter.
--- flattenArray/flattenArray.ts
+++ flattenArray/flattenArray.ts
@@ -1,4 +1,22 @@
-export function flattenArray(arr: any[]): any[] {}
+export function flattenArray(array: any[]): any[] {
+  const flattenedArray = [];
 
-// console.log(flattenArray([[["a"]], [["b"]]]));
-// console.log(flattenArray([1, [2], [3, [[4]]]]));
+  flatten(array);
+
+  function flatten(arr) {
+    for (let index = 0; index < arr.length; index++) {
+      const element = arr[index];
+
+      if (!Array.isArray(element)) {
+        flattenedArray.push(element);
+      } else {
+        flatten(element);
+      }
+    }
+  }
+
+  return flattenedArray;
+}
+
+console.log(flattenArray([[["a"]], [["b"]]]));
+console.log(flattenArray([1, [2], [3, [[4]]]]));
