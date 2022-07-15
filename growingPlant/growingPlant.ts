7487e8979fbce0a9be561d85f05bb73f52bee4da
Alexander <Alexander_alatorre1993@hotmail.com>
2022-07-14 19:12:11 -0600

    solved algo:

--- growingPlant/growingPlant.ts
+++ growingPlant/growingPlant.ts
@@ -1,5 +1,14 @@
-export function growingPlant(upSpeed: number, downSpeed: number, desiredHeight: number): number {
-
+export function growingPlant(
+  upSpeed: number,
+  downSpeed: number,
+  desiredHeight: number
+): number {
+  //grows by 100, drops by 10
+  const growthPerDay = upSpeed - downSpeed;
+  const growth = desiredHeight / growthPerDay;
+  const growthWithoutDecimals = Math.floor(growth);
+  
+  return growthWithoutDecimals
 }
 
-// console.log(growingPlant(100, 10, 910))
+console.log(growingPlant(100, 10, 910));
