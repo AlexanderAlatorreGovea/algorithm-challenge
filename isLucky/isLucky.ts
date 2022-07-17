cd47ec3eacb5839220c69ed79339cefd8088f807
Alexander <Alexander_alatorre1993@hotmail.com>
2022-07-16 18:31:37 -0600

    solved algo

--- isLucky/isLucky.ts
+++ isLucky/isLucky.ts
@@ -1,4 +1,20 @@
-export function isLucky(n: number): boolean {}
+export function isLucky(n: number): boolean {
+  let firstHalfOfTheTicket = 0;
+  let secondHalfOfTheTicket = 0;
+  const stringifiedNumber = n.toString();
 
-// console.log(isLucky(1230));
-// console.log(isLucky(239017));
+  for (let i = 0; i < stringifiedNumber.length; i++) {
+    const halfOfNumber = stringifiedNumber.length / 2;
+
+    if (i < halfOfNumber) {
+      firstHalfOfTheTicket += +stringifiedNumber[i];
+    } else {
+      secondHalfOfTheTicket += +stringifiedNumber[i];
+    }
+  }
+
+  return firstHalfOfTheTicket === secondHalfOfTheTicket;
+}
+
+console.log(isLucky(1230));
+console.log(isLucky(239017));
