e51a3b08c1b83079ecbb7feb69d9c6696e113923
Alexander <Alexander_alatorre1993@hotmail.com>
2022-07-16 18:40:12 -0600

    solved algo

--- isTandemRepeat/isTandemRepeat.ts
+++ isTandemRepeat/isTandemRepeat.ts
@@ -1,7 +1,14 @@
 export function isTandemRepeat(inputString: string): boolean {
+  const isEven = inputString.length % 2 === 0;
+  const halfOfString = inputString.length / 2;
+  const firstHalfOfString = inputString.slice(0, halfOfString);
+  const secondHalfOfString = inputString.slice(halfOfString);
 
+  if (!isEven) return false;
+
+  return firstHalfOfString === secondHalfOfString;
 }
 
-// console.log(isTandemRepeat('tandemtandem'))
-// console.log(isTandemRepeat('qqq'))
-// console.log(isTandemRepeat('2w2ww'))
+console.log(isTandemRepeat("tandemtandem"));
+console.log(isTandemRepeat("qqq"));
+console.log(isTandemRepeat("2w2ww"));
