f83b2ce42fd07cece4ae877aab592cb8b846f2e9
Alexander <Alexander_alatorre1993@hotmail.com>
2022-07-16 21:05:18 -0600

    solved algo

--- largestNumber/largestNumber.ts
+++ largestNumber/largestNumber.ts
@@ -1,5 +1,11 @@
 export function largestNumber(n: number): number {
-    
+  let largest = "";
+
+  for (let i = 0; i < n; i++) {
+    largest += "9";
+  }
+
+  return +largest
 }
 
-// console.log(largestNumber(2));
+console.log(largestNumber(4));
